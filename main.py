b7bd9709e127fbe638be62feb23b580cf77025fd
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-17 18:43:05 +0200

    Setting up turtle screen

---
+++ main.py
@@ -0,0 +1,12 @@
+import turtle
+import pandas
+
+image = "blank_states_img.gif"
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+screen.setup(width=768, height=491)
+turtle.addshape(image)
+
+
+screen.exitonclick()
