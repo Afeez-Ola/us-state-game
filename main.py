e00ecacad0bf120725995db6fc76f1477830dba8
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-19 11:57:05 +0200

    Solved the plotting issue

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 import turtle
 import pandas
+from scoring import Scoreboard
+
+scoreboard = Scoreboard()
 
 image = "blank_states_img.gif"
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
-# screen.setup(width=720, height=460)
+screen.setup(width=720, height=460)
 screen.addshape(image)
 
 turtle.shape(image)
 
 text_turtle = turtle.Turtle()
+text_turtle.penup()
 game_on = True
-
+attempts = 50
 while game_on:
 
-    answer_text = screen.textinput(title="Guess the State", prompt="What's another state's name? ")
+    answer_text = screen.textinput(title=scoreboard, prompt="What's another state's name? ").lower()
     states_dict = pandas.read_csv("50_states.csv").to_dict()
 
     states = states_dict["state"]
@@ -23,10 +27,17 @@ while game_on:
     y_coor = states_dict["y"]
     found_state_index = 0
     for state in states:
-        if states[state] == answer_text:
+        if (states[state]).lower() == answer_text:
             found_state_index = state
-    text_turtle.penup()
+            attempts -=1
+            print(attempts)
+    if attempts == 0:
+        game_on = False
+
+    text_turtle.hideturtle()
+    text_turtle.setposition(x_coor[found_state_index], y_coor[found_state_index])
     text_turtle.write(answer_text)
-    text_turtle.goto(x_coor[found_state_index], y_coor[found_state_index])
+    print(f"{x_coor[found_state_index]} , {y_coor[found_state_index]}")
+
 
 turtle.mainloop()
