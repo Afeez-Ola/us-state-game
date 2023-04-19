98490ce9ff5e35118c53e85af0f15aeafc79867b
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-19 02:47:39 +0200

    Still on it bro

--- main.py
+++ main.py
@@ -5,20 +5,28 @@ image = "blank_states_img.gif"
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
-screen.setup(width=720, height=460)
+# screen.setup(width=720, height=460)
 screen.addshape(image)
 
 turtle.shape(image)
 
-answer_text = screen.textinput(title="Guess the State", prompt="What's another state's name? ")
-states = pandas.read_csv("50_states.csv")
+text_turtle = turtle.Turtle()
+game_on = True
 
-if states["state"]  == answer_text:
-    print(states.state)
+while game_on:
 
+    answer_text = screen.textinput(title="Guess the State", prompt="What's another state's name? ")
+    states_dict = pandas.read_csv("50_states.csv").to_dict()
 
-
-
-
+    states = states_dict["state"]
+    x_coor = states_dict["x"]
+    y_coor = states_dict["y"]
+    found_state_index = 0
+    for state in states:
+        if states[state] == answer_text:
+            found_state_index = state
+    text_turtle.penup()
+    text_turtle.write(answer_text)
+    text_turtle.goto(x_coor[found_state_index], y_coor[found_state_index])
 
 turtle.mainloop()
