17f263a7cf27ac908dfd902da7057255bd36b585
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-20 16:04:25 +0200

    Refactoring bro

--- main.py
+++ main.py
@@ -2,33 +2,36 @@ import turtle
 import pandas
 from scoring import Score
 
+# Initialize Score class
 scoreboard = Score()
 score = scoreboard.score_board()
 
-image = "blank_states_img.gif"
-attempts = 50
-guessed_state = []
-missing_states = []
-
+# Set up screen
 screen = turtle.Screen()
 screen.title("U.S States Game")
 screen.setup(width=720, height=460)
-screen.addshape(image)
-turtle.shape(image)
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
 
+# Load states data from CSV
 states_dict = pandas.read_csv("50_states.csv").to_dict()
-
 states = states_dict["state"]
 x_coor = states_dict["x"]
 y_coor = states_dict["y"]
 state_list = list(states.values())
-
 x_list = list(x_coor.values())
 y_list = list(y_coor.values())
 
+# Initialize game variables
+attempts = 50
+guessed_state = []
+missing_states = []
+
+# Game loop
 while len(guessed_state) < attempts:
     answer_text = (screen.textinput(title=score, prompt="What's another state's name? ")).title()
     if answer_text == "Exit":
+        # Check for missing states
         for item in state_list:
             if item not in guessed_state:
                 missing_states.append(item)
@@ -36,6 +39,7 @@ while len(guessed_state) < attempts:
         remaining_states.to_csv("remaining_states.csv")
         break
     if answer_text in state_list:
+        # Update turtle with guessed state name
         text_turtle = turtle.Turtle()
         text_turtle.hideturtle()
         text_turtle.penup()
@@ -45,6 +49,4 @@ while len(guessed_state) < attempts:
         guessed_state.append(answer_text)
         score = scoreboard.update_score()
     else:
-        print("nope" + answer_text)
-
-
+        print("Nope: " + answer_text)
