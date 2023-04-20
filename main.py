8c6d7cc4d95f3ddf218a3db26e7ce9b3c89bf66c
Afeez Bolaji <afeezbolajiola@gmail.com>
2023-04-20 15:58:50 +0200

    It's looking good bro

--- main.py
+++ main.py
@@ -7,16 +7,14 @@ score = scoreboard.score_board()
 
 image = "blank_states_img.gif"
 attempts = 50
-correct_guess = []
+guessed_state = []
+missing_states = []
 
 screen = turtle.Screen()
 screen.title("U.S States Game")
 screen.setup(width=720, height=460)
 screen.addshape(image)
 turtle.shape(image)
-
-
-
 
 states_dict = pandas.read_csv("50_states.csv").to_dict()
 
@@ -28,9 +26,14 @@ state_list = list(states.values())
 x_list = list(x_coor.values())
 y_list = list(y_coor.values())
 
-while len(correct_guess) < attempts:
+while len(guessed_state) < attempts:
     answer_text = (screen.textinput(title=score, prompt="What's another state's name? ")).title()
     if answer_text == "Exit":
+        for item in state_list:
+            if item not in guessed_state:
+                missing_states.append(item)
+        remaining_states = pandas.DataFrame({"state": missing_states})
+        remaining_states.to_csv("remaining_states.csv")
         break
     if answer_text in state_list:
         text_turtle = turtle.Turtle()
@@ -39,10 +42,9 @@ while len(correct_guess) < attempts:
         index = state_list.index(answer_text)
         text_turtle.goto(x_list[index], y_list[index])
         text_turtle.write(answer_text)
-        correct_guess.append(answer_text)
+        guessed_state.append(answer_text)
         score = scoreboard.update_score()
     else:
         print("nope" + answer_text)
 
-print(correct_guess)
-# turtle.mainloop()
+
